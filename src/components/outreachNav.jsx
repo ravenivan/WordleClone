2f5b316f54ed1e17b92a20add4e81bfb48517bf2
Ivan Lam <ravenivan1@gmail.com>
2024-06-16 18:54:31 -0400

    moved assets to public

--- src/components/outreachNav.jsx
+++ src/components/outreachNav.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Logo from '../assets/logo.png'
+import Logo from '/assets/logo.png'
 import { faFacebook, faDiscord } from '@fortawesome/free-brands-svg-icons';
 import { RiInstagramFill } from "react-icons/ri";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
